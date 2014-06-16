90ee379a3e1c69fcbbc3e3adcc0ac41dcf415038
Manuel Amunategui <amunategui@gmail.com>
2014-06-16 12:11:58 -0700

    Updating with a more compact version

--- ZipToZip.R
+++ ZipToZip.R
@@ -6,27 +6,14 @@ library(Imap)
 #gdist(ny[2],ny[1],pdx[2],pdx[1],units="miles")
  
 # calculate.distance(10002,97232) # in miles
-calculate.distance <- function(zip1,zip2)
-{
+ZipToZip <- function(zip1,zip2) {
         #gdist(lon.1, lat.1, lon.2, lat.2, units = "nm", a = 6378137.0, b = 6356752.3142, verbose = FALSE)
-
-        if (is.na(zip1))
-                        return (NA)
-        if (is.na(zip2))
-                        return (NA)
-
-
-        if (zip1 == "")
-                        return (NA)
-        if (zip2 == "")
-                        return (NA)
-
-        if (suppressWarnings(!is.na(as.numeric(zip1))) == FALSE || suppressWarnings(!is.na(as.numeric(zip2))) == FALSE)
-                        return (NA)
-
-        somedata = data.frame(postal = c(zip1,zip2))
-        somedata$zip = clean.zipcodes(somedata$postal)
-        somedata = merge(somedata, zipcode, by.x='zip', by.y='zip')
-        return(gdist(somedata$longitude[1], somedata$latitude[1], somedata$longitude[2], somedata$latitude[2], units='miles'))
+        if (suppressWarnings(!is.na(as.numeric(zip1)))==T & suppressWarnings(!is.na(as.numeric(zip2)))==T) {
+                somedata = data.frame(postal = c(zip1,zip2))
+                somedata$zip = clean.zipcodes(somedata$postal)
+                somedata = merge(somedata, zipcode, by.x='zip', by.y='zip')
+                return(gdist(somedata$longitude[1], somedata$latitude[1], somedata$longitude[2], somedata$latitude[2], units='miles'))
+        } else {
+                return (NA)
+        }
 }
- 
